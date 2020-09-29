e9d404af357efd56b6fa273c9de81aea726149aa
Ram <45368708+ramgallagher@users.noreply.github.com>
2020-09-29 11:27:47 -0300

    Add files via upload

---
+++ precache-manifest.1ccfb47edccb5730dc3d286c8c1b563b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6577ba6ed8cd1084f78b5414d51aee8",
+    "url": "/cvrami/index.html"
+  },
+  {
+    "revision": "e99d1bf7ec7719cc2681",
+    "url": "/cvrami/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3b19acf7ec1bf8ab60c1",
+    "url": "/cvrami/static/css/main.a0e693c7.chunk.css"
+  },
+  {
+    "revision": "e99d1bf7ec7719cc2681",
+    "url": "/cvrami/static/js/2.65e2eb37.chunk.js"
+  },
+  {
+    "revision": "6ee87c57e2a7b3f0e0b10d639394c036",
+    "url": "/cvrami/static/js/2.65e2eb37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b19acf7ec1bf8ab60c1",
+    "url": "/cvrami/static/js/main.2373eb3b.chunk.js"
+  },
+  {
+    "revision": "dc3973c70eef5bb3c8b9",
+    "url": "/cvrami/static/js/runtime-main.65da6fc6.js"
+  }
+]);
